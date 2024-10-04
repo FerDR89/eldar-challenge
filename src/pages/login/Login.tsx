fcc9f0e51d048fffdb8317b21055c4c3d08f0a2d
Fernando de Row <ferdr89dev@gmail.com>
2024-10-04 09:51:17 -0300

    feat: add Layout, Home, Login and Singup pages mock

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+function Login() {
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default Login;
