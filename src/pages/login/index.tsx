fca1a9fe08cb2e5c7840fdeaee6830f9ce719442
Fernando de Row <ferdr89dev@gmail.com>
2024-10-04 11:44:09 -0300

    refactor: rename login page file

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 function Login() {
   return (
     <>
-      <Button variant="contained">Hello world</Button>
+      <Button variant="contained">Estamos en el login</Button>
     </>
   );
 }
