5d551b48e31c16076c38097d40f5a1175f7cd291
Fernando de Row <ferdr89dev@gmail.com>
2024-10-04 17:09:26 -0300

    feat: add mockedUsers db

---
+++ src/api/db.ts
@@ -0,0 +1,15 @@
+import { IUser } from "../interfaces/user ";
+export const mockedUsers: IUser[] = [
+  {
+    password: "Test123@",
+    role: "user",
+    userId: "user@test.com",
+    userName: "user@test.com",
+  },
+  {
+    password: "Test123@",
+    role: "admin",
+    userId: "admin@test.com",
+    userName: "admin@test.com",
+  },
+];
