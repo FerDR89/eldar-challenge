74949f4229cf12c7c5e585a0ce50a7b614086360
Fernando de Row <ferdr89dev@gmail.com>
2024-10-06 17:45:07 -0300

    chore: add ReactQuery and configure it

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,20 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/index.ts";
+import { QueryClientProvider } from "@tanstack/react-query";
 import MyRouter from "./router/router.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import queryClient from "./api/configs/queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <CssBaseline />
-        <MyRouter />
-      </Provider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <CssBaseline />
+          <MyRouter />
+        </Provider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
