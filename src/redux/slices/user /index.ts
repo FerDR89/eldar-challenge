ddb29f834859d791fed138172307c69f8a3d8618
Fernando de Row <ferdr89dev@gmail.com>
2024-10-06 14:21:38 -0300

    feat: add userSlice, createUser reducer and update they in store

---
+++ src/redux/slices/user /index.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "../../../interfaces/user ";
+import { RootState } from "../../store";
+
+const initialState: IUser = {
+  userId: "",
+  role: [],
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    createUser: (state, action: PayloadAction<IUser>) => {
+      return {
+        ...state,
+        ...action.payload,
+      };
+    },
+  },
+});
+
+export const { createUser } = userSlice.actions;
+export const selectUser = (state: RootState) => state.user;
+export const userReducer = userSlice.reducer;
