46b9ec8f716df862fd0c826a109eaa5b3ddf6120
Fernando de Row <ferdr89dev@gmail.com>
2024-10-06 17:41:19 -0300

    refactor: rename axios file and move it in a config folder

--- src/api/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const BASE_URL = "'https://jsonplaceholder.typicode.com/posts";
-
-const axiosAPI = axios.create({
-  baseURL: BASE_URL,
-});
-
-export default axiosAPI;
---
+++ src/api/configs/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+const axiosAPI = axios.create({
+  baseURL: BASE_URL,
+});
+
+axios.defaults.headers.post["Content-Type"] = "application/json; charset=UTF-8";
+axios.defaults.headers.put["Content-Type"] = "application/json; charset=UTF-8";
+
+export default axiosAPI;
