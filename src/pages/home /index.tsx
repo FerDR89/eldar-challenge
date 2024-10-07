4ade4a485bf8a55d413240282c4e224bcaef8a9c
Fernando de Row <ferdr89dev@gmail.com>
2024-10-07 12:57:14 -0300

    worInProgress: update home

--- src/pages/home /index.tsx
+++ src/pages/home /index.tsx
@@ -1,13 +1,88 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { logout } from "../../redux/slices/auth";
+import useGetAllPost from "../../hooks/useGetAllPost";
+import { saveAllPost, selectPost, updatePost } from "../../redux/slices/post";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import useCreatePost from "../../hooks/useCreatePost";
+import { selectUser } from "../../redux/slices/user ";
+import { allowedEditFormRoles } from "../../constants ";
 
 const Home = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const posts = useAppSelector(selectPost);
+  const { role: userRole } = useAppSelector(selectUser);
+  const { createPostAsync, isErrorCreatePost, isPendingCreatePost } =
+    useCreatePost();
+
+  const {
+    error: errorGetAllPost,
+    isError: isErrorGetAllPost,
+    isPending: isPendingGetAllPost,
+    data: allPosts,
+  } = useGetAllPost();
+
+  useEffect(() => {
+    dispatch(saveAllPost(allPosts));
+  }, [dispatch, allPosts]);
 
   return (
     <div>
       <p>Home</p>
-      <button onClick={() => dispatch(logout())}>Logout</button>
+
+      <ul>
+        {posts?.map((p) => (
+          <li key={p.title} style={{ marginTop: "10px", marginBottom: "10px" }}>
+            {JSON.stringify(p)}
+          </li>
+        ))}
+      </ul>
+
+      <button
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        dashboard
+      </button>
+
+      {allowedEditFormRoles.some((r) => userRole.includes(r)) && (
+        <>
+          <button
+            onClick={() => {
+              createPostAsync({
+                body: "test",
+                title: "un titulo cualquiera",
+              });
+            }}
+          >
+            Create
+          </button>
+          <button
+            onClick={() => {
+              dispatch(
+                updatePost({
+                  body: "TEST UPDATE",
+                  id: 101,
+                })
+              );
+            }}
+          >
+            update
+          </button>
+        </>
+      )}
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+          navigate("/", { replace: true });
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
