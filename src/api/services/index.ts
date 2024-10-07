f5bac855ba4d4da2da0139f728ecaeb9f71597cf
Fernando de Row <ferdr89dev@gmail.com>
2024-10-07 10:16:45 -0300

    fix: bad path url before use for test

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -4,33 +4,7 @@ import { IPost } from "../../interfaces/post";
 
 export const fetchAllPosts = async () => {
   try {
-    const result = await axios.get("/post?_limit=10");
-    return result;
-  } catch (error) {
-    const { message, status } = error as AxiosError;
-    throw {
-      message,
-      status,
-    };
-  }
-};
-
-export const createPost = async (post: IPost) => {
-  try {
-    const result = await axios.get("/post?_limit=10");
-    return result;
-  } catch (error) {
-    const { message, status } = error as AxiosError;
-    throw {
-      message,
-      status,
-    };
-  }
-};
-
-export const updatePost = async () => {
-  try {
-    const result = await axios.get("/post?_limit=10");
+    const result = await axios.get("/posts?_limit=10");
     return result;
   } catch (error) {
     const { message, status } = error as AxiosError;
