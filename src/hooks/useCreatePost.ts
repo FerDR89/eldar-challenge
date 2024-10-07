6e510b099c761f8b58062b477cd8a3f1e99324ff
Fernando de Row <ferdr89dev@gmail.com>
2024-10-07 12:54:43 -0300

    feat: add useCreatePost hook

---
+++ src/hooks/useCreatePost.ts
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import { useMutation } from "@tanstack/react-query";
+import { createPostService } from "../api/services";
+import { createPost } from "../redux/slices/post";
+import { TCreatePost } from "../interfaces/post";
+import { useAppSelector } from "./useAppSelector";
+import { selectUser } from "../redux/slices/user ";
+
+const useCreatePost = () => {
+  const dispatch = useDispatch();
+  const { userId } = useAppSelector(selectUser);
+
+  const { mutateAsync, isError, error, isPending } = useMutation({
+    mutationFn: (newPost: TCreatePost) =>
+      createPostService({ userId, ...newPost }),
+
+    onSuccess: (result, variables) => {
+      const { id: newPostId } = result;
+      dispatch(createPost({ id: newPostId, userId, ...variables }));
+    },
+  });
+
+  if (isError) {
+    console.error(error);
+  }
+
+  return {
+    createPostAsync: mutateAsync,
+    isErrorCreatePost: isError,
+    isPendingCreatePost: isPending,
+  };
+};
+
+export default useCreatePost;
