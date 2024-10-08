af4d26d51e54698fb8bddd95f1ca6afd27af26c8
Fernando de Row <ferdr89dev@gmail.com>
2024-10-08 07:59:45 -0300

    feat: add Container and Box components and css styles in Layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,20 +1,38 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/molecules/header ";
-import Foooter from "../components/molecules/footer ";
+import Header from "../components/header ";
+import Foooter from "../components/footer ";
+import { Box, colors, Container } from "@mui/material";
+import styles from "./index.module.css";
 
 const Layout = () => {
   return (
-    <>
-      <header>
-        <Header />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <Foooter />
-      </footer>
-    </>
+    <Container
+      maxWidth="xl"
+      sx={{
+        backgroundColor: colors.grey[900],
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: colors.grey[900],
+          minHeight: "100vh",
+          width: "100%",
+          maxWidth: "lg",
+        }}
+      >
+        <header className={styles.header}>
+          <Header />
+        </header>
+        <main className={styles.main}>
+          <Outlet />
+        </main>
+        <footer className={styles.footer}>
+          <Foooter />
+        </footer>
+      </Box>
+    </Container>
   );
 };
 
