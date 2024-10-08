9286427e55ce1f83b2c19d83b78b02d75903ccab
Fernando de Row <ferdr89dev@gmail.com>
2024-10-08 14:38:42 -0300

    refactor: add feed_back_validation constant

--- src/constants /index.ts
+++ src/constants /index.ts
@@ -1,3 +1,13 @@
 import { TRoles } from "../interfaces/roles";
 
-export const allowedEditFormRoles: TRoles[] = ["admin"];
+export const CAN_EDIT_FORM: TRoles[] = ["admin"];
+
+export const FEED_BACK_VALIDATION = Object.freeze({
+  required: "Este campo es requerido",
+  validName: "Este campo no acepta caracteres especiales y/o números",
+  validPassword:
+    "Debes ingresar al menos una mayúscula, un carácter especial y un número",
+  minNameCharacters: "Debes ingresar al menos dos caracteres",
+  minPasswordCharacters: "Debes ingresar al menos ocho caracteres",
+  maxCharacters: "Debes ingresar menos de veinte caracteres",
+});
