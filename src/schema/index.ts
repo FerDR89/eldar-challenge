0fbf077999e7441d40947bcd4a5cc42797fb6182
Fernando de Row <ferdr89dev@gmail.com>
2024-10-08 15:01:44 -0300

    feat: add loginSchme

---
+++ src/schema/index.ts
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+import { FEED_BACK_VALIDATION } from "../constants ";
+
+export const loginSchema = yup
+  .object({
+    userName: yup
+      .string()
+      .required(FEED_BACK_VALIDATION.required)
+      .matches(/^[a-zA-Z\s]+$/, FEED_BACK_VALIDATION.validName)
+      .min(2, FEED_BACK_VALIDATION.minNameCharacters)
+      .max(20, FEED_BACK_VALIDATION.maxCharacters)
+      .trim(),
+
+    password: yup
+      .string()
+      .required(FEED_BACK_VALIDATION.required)
+      .matches(
+        /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).*$/,
+        FEED_BACK_VALIDATION.validPassword
+      )
+      .min(8, FEED_BACK_VALIDATION.minPasswordCharacters)
+      .max(20, FEED_BACK_VALIDATION.maxCharacters),
+  })
+  .required();
