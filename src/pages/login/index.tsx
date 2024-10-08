56a15c34291fa47db1ff6ce7325c03a3c134ff54
Fernando de Row <ferdr89dev@gmail.com>
2024-10-08 15:46:29 -0300

    add UI container, title and login form. Also add useForm hook, input validations and onSubmit callback

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,20 +1,36 @@
 import { useNavigate } from "react-router-dom";
+import { Box, Button, colors, Stack, Typography } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { mockedUsers } from "../../api/mockedUsers";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { login } from "../../redux/slices/auth";
 import { createUser } from "../../redux/slices/user ";
-import { mockedUsers } from "../../api/db/db";
+import Input from "../../components/input";
+import { IUserMock } from "../../interfaces/user ";
+import { loginSchema } from "../../schema";
+
+type TLoginForm = Pick<IUserMock, "userName" | "password">;
 
 function Login() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const onSubmit = (e: any) => {
-    e.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<TLoginForm>({
+    mode: "onSubmit",
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit: SubmitHandler<TLoginForm> = (formData) => {
     try {
       const foundUser = mockedUsers.find(
         (u) =>
-          u.userName === e.target.userName.value &&
-          u.password === e.target.password.value
+          u.userName === formData.userName && u.password === formData.password
       );
 
       if (!foundUser) throw new Error("usuario no encontrado");
@@ -32,18 +48,64 @@ function Login() {
       navigate("/home", { replace: true });
     } catch (error) {
       console.log(error);
+    } finally {
+      reset();
     }
   };
 
   return (
-    <>
-      <h1>TITLE</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="userName" />
-        <input type="password" name="password" />
-        <button type="submit">Enviar</button>
-      </form>
-    </>
+    <Stack
+      direction={{ md: "row", lg: "row", xl: "row" }}
+      sx={{
+        height: "85vh",
+        py: "20px",
+        justifyContent: {
+          md: "space-evenly",
+          lg: "space-evenly",
+          xl: "space-evenly",
+        },
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h2" component="h1" color={colors.grey[400]}>
+        Bienvenidos
+      </Typography>
+      <Stack height="100%" justifyContent="center">
+        <Box
+          autoComplete="off"
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          maxWidth="360px"
+        >
+          <Input
+            label="Ingresá tu usuario"
+            type="text"
+            name="userName"
+            helperText={errors.userName?.message}
+            error={Boolean(errors.userName)}
+            register={register}
+          />
+          <Input
+            label="Ingresá tu contraseña"
+            type="password"
+            name="password"
+            helperText={errors.password?.message}
+            register={register}
+            error={Boolean(errors.password)}
+          />
+
+          <Button
+            variant="outlined"
+            color="secondary"
+            type="submit"
+            fullWidth
+            sx={{ color: colors.grey[500] }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Stack>
+    </Stack>
   );
 }
 
