34d8eb6ee66bf4ac6ed41c5c397d6df91abe9754
Fernando de Row <ferdr89dev@gmail.com>
2024-10-08 14:36:06 -0300

    chore: remove db folder and rename db for mockedUsers

--- src/api/mockedUsers.ts
+++ src/api/mockedUsers.ts
@@ -1,16 +1,16 @@
-import { IUserMock } from "../../interfaces/user ";
+import { IUserMock } from "../interfaces/user ";
 
 export const mockedUsers: IUserMock[] = [
   {
     password: "Test123@",
     role: ["user"],
-    userName: "user@test.com",
+    userName: "userTest",
     userId: 1,
   },
   {
     password: "Test123@",
     role: ["admin"],
-    userName: "admin@test.com",
+    userName: "adminTest",
     userId: 2,
   },
 ];
