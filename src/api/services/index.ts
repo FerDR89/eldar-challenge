0cad6ea6e6f346f6b484e6a0638610005294bfe4
Fernando de Row <ferdr89dev@gmail.com>
2024-10-09 09:35:18 -0300

    feat: add updatePostService

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from "axios";
 import axios from "../configs/axios";
-import { TCreatePostService } from "../../interfaces/post";
+import { TCreatePostService, TUpdatePost } from "../../interfaces/post";
 
 export const getAllPostsService = async () => {
   try {
@@ -19,6 +19,41 @@ export const createPostService = async (post: TCreatePostService) => {
   const body = JSON.stringify(post);
   try {
     const result = await axios.post("/posts", body);
+    return result.data;
+  } catch (error) {
+    const { message, status } = error as AxiosError;
+    throw {
+      message,
+      status,
+    };
+  }
+};
+export const updatePostService = async (post: TUpdatePost) => {
+  const { id } = post;
+
+  let body = {};
+
+  if (post.title && post.body) {
+    body = JSON.stringify({
+      body: post.body,
+      title: post.title,
+    });
+  }
+
+  if (post.title) {
+    body = JSON.stringify({
+      title: post.title,
+    });
+  }
+
+  if (post.body) {
+    body = JSON.stringify({
+      body: post.body,
+    });
+  }
+
+  try {
+    const result = await axios.patch(`/posts/${id}`, body);
     return result.data;
   } catch (error) {
     const { message, status } = error as AxiosError;
